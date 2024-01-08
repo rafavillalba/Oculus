67e22742bbaa3a49dd526983ce5f925a4b895be2
rafavillalba <154255338+rafavillalba@users.noreply.github.com>
2024-01-08 17:48:46 +0100

    Revert "cambiado nombre de la clase por Shoot"
    This reverts commit ac96f6b1d227b23bca65ce35b8151a47a8380332.

--- Shoot.cs
+++ Shoot.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Shoot : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
     public GameObject bullet;
     public float shootForce;
