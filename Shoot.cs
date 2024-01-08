d31372fd6e533212c51a70dad23f117aef624912
rafavillalba <154255338+rafavillalba@users.noreply.github.com>
2024-01-08 17:40:52 +0100

    Add files via upload

---
+++ Shoot.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject bullet;
+    public float shootForce;
+    public Transform shootPoint;
+
+    private void Update()
+    {
+        if (GetComponent<OVRGrabbable>().isGrabbed)  
+            if (OVRInput.Get(OVRInput.Button.SecondaryHandTrigger))       
+                if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger))
+                {
+                    Instantiate(bullet, shootPoint.position, shootPoint.rotation).GetComponent<Rigidbody>().AddForce(shootPoint.forward * shootForce);
+                }
+        
+        
+    }
+}
