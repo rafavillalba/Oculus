ac96f6b1d227b23bca65ce35b8151a47a8380332
rafavillalba <154255338+rafavillalba@users.noreply.github.com>
2024-01-08 17:43:42 +0100

    cambiado nombre de la clase por Shoot

--- Shoot.cs
+++ Shoot.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Shoot : MonoBehaviour
 {
     public GameObject bullet;
     public float shootForce;
